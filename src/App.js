4aa0b4c11000a566e16609f6ed3818bd709a97a0
myGitName <1746657548@qq.com>
2022-01-15 14:13:38 +0800

    第三次

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import logo from './logo.svg';
-import './App.css'
-import { Button } from 'antd';
-import Admin from './pages/Home';
-import Login from './pages/Login';
+import './App.less'
+import Admin from './pages/Admin/admin';
+import Login from './pages/Login/login';
 import { Switch, Route, NavLink } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <Button type="primary">Primary Button</Button>
       <Switch>
         <Route path='/login' component={Login}></Route>
-        <Route path='/admin' component={Admin}></Route>
+        <Route path='/' component={Admin}></Route>
       </Switch>
     </div>
   );
---
+++ src/pages/Login/login.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './login.less'
+
+export default class Login extends Component {
+    render() {
+        return (
+            <div className='login'>
+                <header>login</header>
+                <article className='login-artitle'>.....</article>
+            </div>
+        )
+    }
+}
